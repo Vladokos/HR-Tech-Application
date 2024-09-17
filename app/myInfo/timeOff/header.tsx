3a3f24f72180408bddb98978df661ccb6d830365
Vlad <vladnothepaver@gmail.com>
2024-09-17 15:27:19 +0400

    added title for first burger

--- app/myInfo/timeOff/header.tsx
+++ app/myInfo/timeOff/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { Search, Settings, CircleHelp, Bell, Menu } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "@/components/ui/sheet";
 
 interface HeaderInterface {
   avatar: string;
@@ -67,6 +67,9 @@ export default function Header({ avatar }: HeaderInterface) {
             </div>
             {/* burger */}
             <Sheet>
+              <SheetTitle className="p-0 m-0">
+
+              </SheetTitle>
               <SheetTrigger>
                 <Menu className="hidden max-[1200px]:block max-[1200px]:mb-4" />
               </SheetTrigger>
